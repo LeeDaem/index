ce6a4c7bf333ea7ebb3db57f9604560b50e62b70
LeeDaem <dleorud1296@gmail.com>
2026-05-28 11:46:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const todoInput = document.querySelector("#todo-input");
-const addBtn = document.querySelector("#add-btn");
-const todoList = document.querySelector("#todo-list");
+const todoInput = document.querySelector(".todo-input");
+const addBtn = document.querySelector(".add-btn");
+const todoList = document.querySelector(".todo-list");
 
     
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
